0f9f74623fd3db2c30ba07372135668bd6a839ef
Andrii Kosteniuk <andriy.kosteniuk@gmail.com>
2023-11-02 15:48:21 +0100

    Ok

--- src/main/java/com/epam/autotasks/Cat.java
+++ src/main/java/com/epam/autotasks/Cat.java
@@ -3,9 +3,11 @@ package com.epam.autotasks;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.function.Supplier;
+
 @Data
 @Builder
-public class Cat {
+public class Cat  {
 
     private String name;
     private Integer age;
